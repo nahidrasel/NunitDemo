4c2bd03d3af57aa3939c5cfa18915e5075bdadfe
nhdrasel <nhdrasel@gmail.com>
2022-03-14 09:15:45 +1300

    Conflict Commit

--- NunitDemo/NunitDemo/ConflictTest.cs
+++ NunitDemo/NunitDemo/ConflictTest.cs
@@ -13,12 +13,14 @@ namespace NunitDemo
 			[Test]
 			public void CTest()
 			{
+			
 				Console.WriteLine("This is Conlict Commit By Nahid QA");
-			Console.WriteLine("Empty");
+			
+				Console.WriteLine("Empty");
 
-			Console.WriteLine("Conflict By Nahid Original");
+				Console.WriteLine("Conflict By Nahid Original");
 
-			Console.WriteLine("Conflict By QA");
+				Console.WriteLine("Conflict By QA");
 
 				Assert.Pass();
 
