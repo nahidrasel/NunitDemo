c779dcbff9d3fa47e5fafff4564cc63e854c979b
nahidmahmudqa <nahid.mahmud.qa@gmail.com>
2022-03-13 14:55:07 +1300

    Third Commit

---
+++ NunitDemo/NunitDemo/UnitTest3.cs
@@ -0,0 +1,21 @@
+﻿	using NUnit.Framework;
+	using System;
+
+	namespace NunitDemo
+	{
+		public class Test3
+		{
+			[SetUp]
+			public void Setup()
+			{
+			}
+
+			[Test]
+			public void Test2()
+			{
+				Console.WriteLine("This is Third Commit");
+				Assert.Pass();
+
+			}
+		}
+	}
