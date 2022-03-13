d918a6b83891ce45db6120804269f71cec9fdb3d
nahidmahmudqa <nahid.mahmud.qa@gmail.com>
2022-03-13 14:41:36 +1300

    Second Commit QA

---
+++ NunitDemo/NunitDemo/UnitTest2.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NunitDemo
+{
+	public class TestFor2
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test2()
+		{
+			Console.WriteLine("This is Second Commit");
+			Assert.Pass();
+
+		}
+	}
+}
